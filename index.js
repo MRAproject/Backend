c28a0b20f1ed099da061f711e1945d71c7da7189
Amit <amit@herolo.co.il>
2019-02-22 10:13:43 +0200

    Add headers

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const config = require('./src/shared/config');
+const login = require('./src/api/login');
 
 const app = express();
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", config.allowOriginUri);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Access-Control-Allow-Credentials", 'true');
+    next();
+});
+
+app.post('/login', login);
+
+app.listen(config.apiPort, () => console.log(`The API is listening on port ${config.apiPort}!`));

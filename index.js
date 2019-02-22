4ed14bb6faa2c0d724d054a0bc72c7efb9ed6f8b
Amit <amit@herolo.co.il>
2019-02-22 13:03:23 +0200

    Add new endpoint

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const config = require('./src/shared/config');
 const login = require('./src/api/login');
 const add_car = require('./src/api/add_car');
+const get_all_user_cars = require('./src/api/get_all_user_cars');
 const DBM = require('./src/db/DBM');
 
 const app = express();
@@ -18,6 +19,7 @@ app.use((req, res, next) => {
 
 app.post('/login', login);
 app.post('/add_car', add_car);
+app.get('/get_all_user_cars',get_all_user_cars);
 
 async function resetTable() {
     const dbm = new DBM();

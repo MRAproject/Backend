9c9cad54d8aaf0686981eebb3baa62ccf2cf4fb5
Amit Markovich <amitmarkovich92@gmail.com>
2019-03-08 15:04:04 +0200

    Add check car

--- src/api/check_car.js
+++ src/api/check_car.js
@@ -1,25 +1,25 @@
-const DBM = require('../db/DBM');
+const DBM = require("../db/DBM");
 
 const check_car = async (req, res) => {
-    const { carNumber } = req.query;
-    if (!carNumber) {
-        return res.status(403).json({
-            status: 'unauthorized'
-        });
-    }
-    const dbm = new DBM();
-    await dbm.open();
-    const car = await dbm.checkCar([carNumber]);
-    const status = car.length > 0 ? 1 : 0;
-    if (status) {
-        await dbm.updateCarInside([1, carNumber, 'amitmarko']);
-    }
-    await dbm.close();
-    console.log('car', car);
-    res.json({
-        status
+  const { carNumber } = req.query;
+  if (!carNumber) {
+    return res.status(403).json({
+      status: "unauthorized"
     });
-
-}
+  }
+  const dbm = new DBM();
+  await dbm.open();
+  const car = await dbm.checkCar([carNumber]);
+  const status = car.length > 0 ? 1 : 0;
+  if (status) {
+    const isInside = car[0].isInside === 1 ? 0 : 1;
+    await dbm.updateCarInside([isInside, carNumber, "amitmarko"]);
+  }
+  await dbm.close();
+  console.log("car", car);
+  res.json({
+    status
+  });
+};
 
 module.exports = check_car;
